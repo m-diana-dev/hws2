7ba5540560caf2ec1e5130961649e152079ad3b5
m-diana-dev <m.diana.dev@gmail.com>
2023-05-18 14:37:12 +0300

    homework-4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,21 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
+    // const finalClassName = s.button
         // + (disabled
         //         ? ...
         //         : xType === 'red'
         //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        // + (className ? ' ' + className : '')
+    // задачка на смешивание классов
+
+    const finalClassName = s.button
+        + ' ' + (disabled
+            ? s.disabled
+            : xType === 'secondary'
+                ? s.secondary : s.default)
+        + ' ' + (xType === 'red' ? '' + s.red : '')
+
 
     return (
         <button
