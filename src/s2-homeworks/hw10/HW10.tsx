21496a6c1f60f85bed6dabf88a8e1c8d34515e21
m-diana-dev <m.diana.dev@gmail.com>
2023-10-02 16:59:41 +0300

    homework-10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -15,16 +15,21 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const loading = useSelector<AppStoreType, {isLoading: boolean}>(state => state.loading)
+    const dispatch = useDispatch()
+
+    const isLoading = loading.isLoading
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
 
-        // setTimeout
+        dispatch(loadingAC(true))
     }
 
     return (
-        <div id={'hw10'}>
+        <div id={'hw10'} className={s2.hwWrapp}>
             <div className={s2.hwTitle}>Homework #10</div>
 
             <div className={s2.hw}>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,13 @@
+import {AppStoreType} from "./store";
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: {isLoading: boolean} = initState, action: LoadingActionType): {isLoading: boolean} => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
+        case "CHANGE_LOADING":
+            return {...state, isLoading: action.isLoading}
 
         default:
             return state
